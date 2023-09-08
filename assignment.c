eedcb4b0c26eeae2c9eb97e3b4b4c8ced97282be
tom-wells46 <thomas.a.wells-1@ou.edu>
2023-09-08 08:24:53 -0500

    Add array multiplication and tests

--- assignment.c
+++ assignment.c
@@ -8,3 +8,12 @@ int add_arr(int* arr) {
 	}
 	return result;
 }
+
+int mult_arr(int* arr) {
+	int result = 1;
+	while (*arr) {
+		result *= *arr;
+		arr++;
+	}
+	return result;
+}
--- tester.c
+++ tester.c
@@ -9,11 +9,21 @@ int add_arr_test(int* arr, int expected) {
 	return 0;
 }
 
+int mult_arr_test(int* arr, int expected) {
+	int result = mult_arr(arr);
+	if (result == expected) {
+		return 1;
+	}
+	return 0;
+}
+
 int main() {
 	int test1[] = {1, 2, 3, 4, 5, 6};
 	int test2[] = {1, -2, 3, -4, 5};
 
 	printf("add_arr_test1: %b\n", add_arr_test(test1, 21));
 	printf("add_arr_test2: %b\n", add_arr_test(test2, 3));
+	printf("mult_arr_test1: %b\n", mult_arr_test(test1, 720));
+	printf("mult_arr_test2: %b\n", mult_arr_test(test2, 120));
 	return 0;
 }
