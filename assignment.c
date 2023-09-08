3645d8b3309a49cc1f8b943cbbf8e9ac9da3bf66
tom-wells46 <thomas.a.wells-1@ou.edu>
2023-09-08 08:38:32 -0500

    Implement main method in assignment.c

--- assignment.c
+++ assignment.c
@@ -17,3 +17,14 @@ int mult_arr(int* arr) {
 	}
 	return result;
 }
+
+int main() {
+	int arr[5];
+	printf("Please enter 5 numbers\n");
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", (arr+i));
+	}
+
+	printf("The addition of your numbers is: %d\n", add_arr(arr));
+	printf("The multiplication of your numbers is: %d\n", mult_arr(arr));
+}
